00f24450be47bf50eda53e89a2cf485d18af8161
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2025-05-20 17:26:51 +0100

    Initial commit

---
+++ src/main/java/com/mygroup/snakes_and_ladders/SnakesAndLaddersApplication.java
@@ -0,0 +1,13 @@
+package com.mygroup.snakes_and_ladders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakesAndLaddersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakesAndLaddersApplication.class, args);
+	}
+
+}
